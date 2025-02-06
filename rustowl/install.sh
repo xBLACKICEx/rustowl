b60820609d29b5110edd81ec526635a61b7f351d
CHISEN Kaoru <cordx56@cordx.cx>
2025-02-06 17:12:13 +0900

    fix install.sh

--- rustowl/install.sh
+++ rustowl/install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 VERSION="v0.0.5"
 
-function install_rust() {
+install_rust() {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y --profile minimal
 }
 
-function install_rustowl() {
+install_rustowl() {
     if ! which curl > /dev/null 2>&1; then
         echo "curl command not found; error"
         return
@@ -15,11 +15,14 @@ function install_rustowl() {
         echo "unzip command not found; error"
         return
     fi
-    cd "$(mktemp -d)"
+    TMP="$(mktemp -d)"
+    cd "$TMP"
     curl -L "https://github.com/cordx56/rustowl/releases/download/$VERSION/rustowl.zip" > rustowl.zip
     unzip rustowl.zip
     cd ./rustowl
     cargo install --path . --locked
+    cd ../../
+    rm -rf "$TMP"
 }
 
 install_rustowl
