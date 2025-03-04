e93806d3ecb06184469d259d5218cfe34d733759
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-03-04 15:30:01 +0900

    refactor: add prefix to functions with commonly used names

--- emacs/rustowlsp.el
+++ emacs/rustowlsp.el
@@ -33,36 +33,36 @@
 
 (defun rustowlsp-cursor (params)
   (lsp-request-async
-    "rustowl/cursor"
-    params
-    (lambda (response)
-      (let ((decorations (gethash "decorations" response)))
-        (mapc
-          (lambda (deco)
-            (let* ((type (gethash "type" deco))
-                   (start (gethash "start" (gethash "range" deco)))
-                   (end (gethash "end" (gethash "range" deco)))
-                   (start-pos
-                     (line-col-to-pos
-                       (gethash "line" start)
-                       (gethash "character" start)))
-                   (end-pos
-                     (line-col-to-pos
-                       (gethash "line" end)
-                       (gethash "character" end))))
-              (cond
-                ((equal type "lifetime")
-                 (underline start-pos end-pos "#00cc00"))
-                ((equal type "imm_borrow")
-                 (underline start-pos end-pos "#0000cc"))
-                ((equal type "mut_borrow")
-                 (underline start-pos end-pos "#cc00cc"))
-                ((or (equal type "move") (equal type "call"))
-                 (underline start-pos end-pos "#cccc00"))
-                ((equal type "outlive")
-                 (underline start-pos end-pos "#cc0000")))))
-                decorations)))
-    :mode 'current))
+   "rustowl/cursor"
+   params
+   (lambda (response)
+     (let ((decorations (gethash "decorations" response)))
+       (mapc
+        (lambda (deco)
+          (let* ((type (gethash "type" deco))
+                 (start (gethash "start" (gethash "range" deco)))
+                 (end (gethash "end" (gethash "range" deco)))
+                 (start-pos
+                  (rustowlsp-line-col-to-pos
+                   (gethash "line" start)
+                   (gethash "character" start)))
+                 (end-pos
+                  (rustowlsp-line-col-to-pos
+                   (gethash "line" end)
+                   (gethash "character" end))))
+            (cond
+             ((equal type "lifetime")
+              (rustowlsp-underline start-pos end-pos "#00cc00"))
+             ((equal type "imm_borrow")
+              (rustowlsp-underline start-pos end-pos "#0000cc"))
+             ((equal type "mut_borrow")
+              (rustowlsp-underline start-pos end-pos "#cc00cc"))
+             ((or (equal type "move") (equal type "call"))
+              (rustowlsp-underline start-pos end-pos "#cccc00"))
+             ((equal type "outlive")
+              (rustowlsp-underline start-pos end-pos "#cc0000")))))
+        decorations)))
+   :mode 'current))
 
 
 (defun rustowlsp-line-number-at-pos ()
@@ -98,9 +98,9 @@
 (defun rustowlsp-reset-cursor-timer ()
   (when rustowlsp-cursor-timer
     (cancel-timer rustowlsp-cursor-timer))
-  (clear-overlays)
+  (rustowlsp-clear-overlays)
   (setq rustowlsp-cursor-timer
-    (run-with-idle-timer rustowlsp-cursor-timeout nil #'rustowlsp-cursor-call)))
+        (run-with-idle-timer rustowlsp-cursor-timeout nil #'rustowlsp-cursor-call)))
 
 ;;;###autoload
 (defun enable-rustowlsp-cursor ()
@@ -117,19 +117,22 @@
 (enable-rustowlsp-cursor)
 
 ;; RustOwl visualization
-(defun line-col-to-pos (line col)
+(defun rustowlsp-line-col-to-pos (line col)
   (save-excursion
     (goto-char (point-min))
     (forward-line line)
     (move-to-column col)
     (point)))
+
 (defvar rustowl-overlays nil)
-(defun underline (start end color)
+
+(defun rustowlsp-underline (start end color)
   (let ((overlay (make-overlay start end)))
-    (overlay-put overlay 'face `(:underline (:color ,color :style wave)))
+    (overlay-put overlay 'face `(:rustowlsp-underline (:color ,color :style wave)))
     (push overlay rustowl-overlays)
     overlay))
-(defun clear-overlays ()
+
+(defun rustowlsp-clear-overlays ()
   (interactive)
   (mapc #'delete-overlay rustowl-overlays)
   (setq rustowl-overlays nil))
