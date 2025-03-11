a8354cca95129742e94f5a4eb2ccf701ecdb7ad8
Marc Jakobi <marc.jakobi@tiko.energy>
2025-03-11 21:34:04 +0100

    neovim: support didChangeWorkspaceFolders

--- lua/rustowl/lsp.lua
+++ lua/rustowl/lsp.lua
@@ -10,13 +10,91 @@ M.get_rustowl_clients = function(filter)
   return type(vim.lsp.get_clients) == 'function' and vim.lsp.get_clients(filter) or vim.lsp.get_active_clients(filter)
 end
 
+---@param client vim.lsp.Client
+---@param root_dir string
+---@return boolean
+local function is_in_workspace(client, root_dir)
+  if not client.workspace_folders then
+    return false
+  end
+  for _, dir in ipairs(client.workspace_folders) do
+    if (root_dir .. '/'):sub(1, #dir.name + 1) == dir.name .. '/' then
+      return true
+    end
+  end
+  return false
+end
+
+---Compatibility for a breaking change in Nvim 0.11
+--- @param client vim.lsp.Client
+--- @param method string LSP method name.
+--- @param params table? LSP request params.
+--- @return boolean status indicating if the notification was successful.
+---                        If it is false, then the client has shutdown.
+local function client_notify(client, method, params)
+  local info = debug.getinfo(client.notify, 'u')
+  if info.nparams > 0 then
+    ---@diagnostic disable-next-line: param-type-mismatch
+    return client:notify(method, params)
+  else
+    ---@diagnostic disable-next-line: param-type-mismatch
+    return client.notify(method, params)
+  end
+end
+
+---@param client vim.lsp.Client
+---@param root_dir string
+local function notify_workspace_folder(client, root_dir)
+  if is_in_workspace(client, root_dir) then
+    return
+  end
+  local workspace_folder = { uri = vim.uri_from_fname(root_dir), name = root_dir }
+  local params = {
+    event = {
+      added = { workspace_folder },
+      removed = {},
+    },
+  }
+  client_notify(client, 'workspace/didChangeWorkspaceFolders', params)
+  if not client.workspace_folders then
+    client.workspace_folders = {}
+  end
+  table.insert(client.workspace_folders, workspace_folder)
+end
+
 ---Start / attach the LSP client
 ---@return integer|nil client_id The LSP client ID
 M.start = function()
   local config = require('rustowl.config')
   local root_dir = config.client.root_dir()
+  if not root_dir then
+    vim.schedule(function()
+      vim.notify('rustowl: Failed to detect root_dir.', vim.log.levels.ERROR)
+    end)
+    return
+  end
+
+  for _, client in ipairs(M.get_rustowl_clients()) do
+    if not is_in_workspace(client, root_dir) then
+      -- Attach to existing session
+      notify_workspace_folder(client, root_dir)
+      vim.lsp.buf_attach_client(0, client.id)
+      return
+    end
+  end
+
+  ---@param client vim.lsp.Client
+  local notify_workspace_folder_hook = function(client)
+    notify_workspace_folder(client, root_dir)
+  end
+  local on_init = type(config.client.on_init) == 'function'
+      and function(...)
+        notify_workspace_folder_hook(...)
+        config.client.on_init(...)
+      end
+    or notify_workspace_folder_hook
   ---@type vim.lsp.ClientConfig
-  local client_config = vim.tbl_deep_extend('force', config.client, { root_dir = root_dir })
+  local client_config = vim.tbl_deep_extend('force', config.client, { root_dir = root_dir, on_init = on_init })
   return vim.lsp.start(client_config)
 end
 
