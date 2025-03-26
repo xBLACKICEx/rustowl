71d63bc1e501d6ef6b81a64f6a4d477507d93705
CHISEN Kaoru <cordx56@cordx.cx>
2025-03-26 06:13:39 +0000

    build without rustup

--- rustowl/build.rs
+++ rustowl/build.rs
@@ -3,13 +3,15 @@ use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 fn main() {
-    let mut stdout = Command::new("rustup")
+    let child = match Command::new("rustup")
         .args(["show", "active-toolchain"])
         .stdout(Stdio::piped())
         .spawn()
-        .unwrap()
-        .stdout
-        .unwrap();
+    {
+        Ok(v) => v,
+        Err(_) => return,
+    };
+    let mut stdout = child.stdout.unwrap();
     let mut output = String::new();
     stdout.read_to_string(&mut output).unwrap();
     let toolchain = output.split("\n").nth(0).unwrap().trim();
