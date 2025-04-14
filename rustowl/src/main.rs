2ff37d1cf04b462d67fa4d616db84082c5db84c1
CHISEN Kaoru <cordx56@cordx.cx>
2025-04-14 14:01:45 +0900

    use jemalloc

--- rustowl/src/main.rs
+++ rustowl/src/main.rs
@@ -23,6 +23,39 @@ pub mod core;
 use std::process::exit;
 
 fn main() {
+    // This is cited from [rustc](https://github.com/rust-lang/rust/blob/1.86.0/compiler/rustc/src/main.rs).
+    // MIT License
+    #[cfg(feature = "jemalloc")]
+    {
+        use std::os::raw::{c_int, c_void};
+
+        use tikv_jemalloc_sys as jemalloc_sys;
+
+        #[used]
+        static _F1: unsafe extern "C" fn(usize, usize) -> *mut c_void = jemalloc_sys::calloc;
+        #[used]
+        static _F2: unsafe extern "C" fn(*mut *mut c_void, usize, usize) -> c_int =
+            jemalloc_sys::posix_memalign;
+        #[used]
+        static _F3: unsafe extern "C" fn(usize, usize) -> *mut c_void = jemalloc_sys::aligned_alloc;
+        #[used]
+        static _F4: unsafe extern "C" fn(usize) -> *mut c_void = jemalloc_sys::malloc;
+        #[used]
+        static _F5: unsafe extern "C" fn(*mut c_void, usize) -> *mut c_void = jemalloc_sys::realloc;
+        #[used]
+        static _F6: unsafe extern "C" fn(*mut c_void) = jemalloc_sys::free;
+
+        #[cfg(target_os = "macos")]
+        {
+            unsafe extern "C" {
+                fn _rjem_je_zone_register();
+            }
+
+            #[used]
+            static _F7: unsafe extern "C" fn() = _rjem_je_zone_register;
+        }
+    }
+
     simple_logger::SimpleLogger::new()
         .env()
         .with_colors(true)
