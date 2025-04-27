28f52081984e02818961e99ec65dde2fd8c28dc8
CHISEN Kaoru <cordx56@cordx.cx>
2025-04-27 14:47:57 +0900

    prepare 0.3.0 (0.3.0-alpha.1)

--- example/all.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut string = String::from("A");
-    let reference = &string;
-    string.push_str("B");
-    println!("{}", reference);
-    string.into_bytes();
-    println!("{}", string);
-}
